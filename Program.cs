a5932ca7d7afa736716e00da57930b77dc3e24d9
kalukwo <kaldrack789@gmail.com>
2025-07-30 14:05:36 +0300

    add redis ceonnection

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 using FileHandler.Data;
 using FileHandler.Services;
 using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;  // Redis
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add Redis connection - ADD THIS
+builder.Services.AddSingleton<IConnectionMultiplexer>(provider =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("Redis") ?? "redis:6379";
+    return ConnectionMultiplexer.Connect(connectionString);
+});
+
 builder.Services.AddScoped<IFileService, FileService>();
 
 
